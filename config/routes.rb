b55bc14533fbf5e9c8e46512e8eabe7e7adca3d7
reepz <a.wojtowicz@outlook.com>
2017-06-10 14:24:26 +0200

    add ringtones, changes font

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/contact'
 
+  get 'static_pages/ringtones'
+
   root 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
