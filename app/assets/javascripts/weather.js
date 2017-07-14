ec53fa233191cdc38a6a887c4c575a74570c3d40
reepz <a.wojtowicz@outlook.com>
2017-07-14 09:37:23 +0200

    modifies weather functionality

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -2,26 +2,15 @@ var conditions = ['clear sky', 'few clouds', 'scattered clouds', 'broken clouds'
 
 var city;
 var country;
+var lat;
+var lon;
 
  $.getJSON("http://ip-api.com/json", function(json) {
-
    country = json.country;
    city = json.city;
+   lat = json.lat;
+   lon = json.lon;
+ });
 
-   $(".location").text(city + ", " + country);
-   //  nested json ajax calls - bad?
-   $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + key, function(json) {
-
-     temperature = json.main.temp;
-     description = json.weather['0'].description;
-
-     $("#temperature").text("Current temperature: " + temperature);
-     $("#description").text(description);
-   });
-});
-
-
-
-var key = '5b9f5435728ffd57f83e2571b497d1d2';
 var temperature;
 var description;
