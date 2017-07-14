c990c674fb114ebc6bac16d87fea31fa0833d743
reepz <a.wojtowicz@outlook.com>
2017-07-14 10:17:00 +0200

    ajax calls work

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -5,11 +5,19 @@ var country;
 var lat;
 var lon;
 
+var temperature;
+
  $.getJSON("http://ip-api.com/json", function(json) {
    country = json.country;
    city = json.city;
    lat = json.lat;
    lon = json.lon;
+
+   $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat="+ lat + "&lon=" + lon "?output=jsonp&callback=?", function(json) {
+     temperature = json.main.temp;
+     $("#temperature").append(temperature);
+     console.log(temperature);
+   });
  });
 
 var temperature;
