4a58d32ce314a3d366f930070ec79b28732f86f5
reepz <a.wojtowicz@outlook.com>
2017-07-25 00:54:23 +0200

    improves ajax call for weather

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -7,29 +7,16 @@ var lon;
 
 var temperature;
 
- $.getJSON("http://ip-api.com/json", function(json) {
-   country = json.country;
-   city = json.city;
-   lat = json.lat;
-   lon = json.lon;
+navigator.geolocation.getCurrentPosition(function(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
 
-   $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat="+ lat + "&lon=" + lon, function(jsonp) {
-     temperature = jsonp.main.temp;
-     $("#temperature").append(temperature);
-     $("#description").append(city);
-     console.log(temperature);
-     console.log(lat);
-     console.log(lon);
-   });
-
-      //
-      // $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=19&APPID=5b9f5435728ffd57f83e2571b497d1d2", function(jsonp) {
-      //   temperature = jsonp.main.temp;
-      //   $("#temperature").append(temperature);
-      //   $("#description").append(city);
-      //   console.log(temperature);
-      //   console.log(lat);
-      //   console.log(lon);
-      // });
-
- });
+  $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat="+ lat + "&lon=" + lon, function(jsonp) {
+    temperature = jsonp.main.temp;
+    $("#temperature").append(temperature);
+    $("#description").append(city);
+    console.log(temperature);
+    console.log(lat);
+    console.log(lon);
+  });
+});
